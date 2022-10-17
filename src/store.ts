106c449783794031f90a38c76e2facd579d60486
Fin Hendrik Bahnsen <fin.bahnsen@tuhh.de>
2022-10-17 16:44:15 +0200

    permission flags

--- src/store.ts
+++ src/store.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 const PATH = "store.json"
 
 const init = fs.existsSync(PATH)
-    ? JSON.parse(fs.readFileSync(PATH, {encoding: "utf-8"}))
+    ? JSON.parse(fs.readFileSync(PATH, {encoding: "utf-8", flag: "r"}))
     : {
         last_value_at: 0,
     };
@@ -14,7 +14,7 @@ const store = new Proxy(init, {
     set: (target, prop, value) => {
         target[prop] = value;
         console.log("[store] update ...", target)
-        fs.writeFileSync(PATH, JSON.stringify(target), {encoding: "utf-8", flag: "wx"})
+        fs.writeFileSync(PATH, JSON.stringify(target), {encoding: "utf-8", flag: "w"})
         return true;
     },
 });
