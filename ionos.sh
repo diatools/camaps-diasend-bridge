9a3309fa84ab31d03a40e6a4a050116faeb3a38b
diatools <111637249+diatools@users.noreply.github.com>
2022-10-21 18:57:45 +0200

    Update ionos.sh

--- ionos.sh
+++ ionos.sh
@@ -2,15 +2,15 @@
 
 cd $HOME/nightscout
 
-if -d "camaps-diasend-bridge"; then
+if test -d "camaps-diasend-bridge"; then
     echo "#########################\n"
     ehco "camaps-diasend-bridge is allready installed\n"
     echo "nothing to do ....\n"
     echo "GOOD BYE\n"
     echo "#########################\n"
 else
-    echo "CAMAPS=./camaps-diasend-bridge" > .env
-    echo "COMPOSE_FILE=docker-compose.yml:./camaps-diasend-bridge" > .env
+    echo "CAMAPS=./camaps-diasend-bridge" >> .env
+    echo "COMPOSE_FILE=docker-compose.yml:./camaps-diasend-bridge" >> .env
 
     git clone https://github.com/diatools/camaps-diasend-bridge.git camaps-diasend-bridge 
 fi
